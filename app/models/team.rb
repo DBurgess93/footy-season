17dc0a248ffca6836c041ad6c39559a0ba3d2e40
Daniel Burgess <dan-burgess@hotmail.com>
2023-08-26 20:29:49 +0930

    Add Teams total ranking points to ladder

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,8 @@ class Team < ApplicationRecord
   has_many :players
   has_many :participations
   has_many :matches, through: :participations
+
+  def calculate_total_ranking_points
+    players.sum(:ranking)
+  end
 end
